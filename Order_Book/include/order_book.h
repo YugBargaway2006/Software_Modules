a365b6279f085fef6143f5ab29257af78ded03b5
Yug Bargaway <yugbargawayskills@gmail.com>
2025-12-17 15:03:59 +0530

    Added interface contract for order_book class

---
+++ Order_Book/include/order_book.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <map>
+#include <list>
+#include <string>
+#include <memory>
+#include <unordered_map>
+#include "order.h"
+
+
+class Order_Book {
+private:
+    std::map<double, std::list<std::shared_ptr<Order>>, std::greater<double>> bids;   // Take the highest Bid First
+    std::map<double, std::list<std::shared_ptr<Order>>, std::less<double>> asks;      // Take the lowest ask first
+    std::unordered_map<int, std::shared_ptr<Order>> lookup_table;
+
+    void match_buy_sell();
+
+public:
+    Order_Book() {}
+
+    void addOrder(const int& id, const int& quantity, const double& price, const std::string& side);
+    bool cancelOrder(const int& id);   // Returns true if successful
+
+    const double getBestBid() const;
+    const double getBestAsk() const;
+
+    void printOrderBook() const;
+};
