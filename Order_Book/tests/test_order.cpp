8f95b1885aaf46371f7c721b9ff2b371d0cbd1f9
Yug Bargaway <yugbargawayskills@gmail.com>
2025-12-17 15:03:30 +0530

    Added test for order class

---
+++ Order_Book/tests/test_order.cpp
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+#include "../include/order.h"
+#include "../include/error.h"
+
+
+/**
+ * @brief test if Order object is initialized correctly.
+ */
+TEST(OrderUnitTest, NewOrderInitialisesCorrectly) {
+    Order stock(1, 10, 100.15, "bid");
+
+    EXPECT_EQ(stock.quantity, 10);
+    EXPECT_DOUBLE_EQ(stock.price, 100.15);
+}
+
+
+/**
+ * @brief test if throws value_error if value of num is greater than quantity.
+ */
+TEST(OrderUnitTest, ThrowsErrorForMoreRetrieval) {
+    Order stock(1, 10, 100.15, "bid");
+
+    EXPECT_THROW(stock.buy_some(20), std::runtime_error);
+}
+
+
+/**
+ * @brief test if throws value_error if value of num is negative.
+ */
+TEST(OrderUnitTest, ThrowsErrorForNegativeRetrieval) {
+    Order stock(1, 10, 100.15, "bid");
+
+    EXPECT_THROW(stock.buy_some(-20), std::runtime_error);
+}
+
+
+/**
+ * @brief test if gives correct price for buying x quantity.
+ */
+TEST(OrderUnitTest, ThrowsErrorForMoreRetrieval) {
+    Order stock(1, 10, 100.15, "bid");
+
+    EXPECT_DOUBLE_EQ(stock.buy_some(5), 500.75);
+}
+
+
+/**
+ * @brief test if gives correct side of the stock for bid.
+ */
+TEST(OrderUnitTest, CorrectBidCategory) {
+    Order stock(1, 10, 100.15, "bid");
+
+    EXPECT_EQ(stock.Side(), (const std::string)"bid");
+}
+
+
+/**
+ * @brief test if gives correct side of the stock for ask.
+ */
+TEST(OrderUnitTest, CorrectBidCategory) {
+    Order stock(1, 10, 100.15, "ask");
+
+    EXPECT_EQ(stock.Side(), (const std::string)"ask");
+}
+
+
+/**
+ * @brief test if gives correct ID retrieval of the stock.
+ */
+TEST(OrderUnitTest, CorrectIdRetrieval) {
+    Order stock(1, 10, 100.15, "ask");
+
+    EXPECT_EQ(stock.Id(), 1);
+}
+
+
+
