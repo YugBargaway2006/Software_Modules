26f9b300d6e5b3ccfe37745aedb35670e8f49162
Yug Bargaway <yugbargawayskills@gmail.com>
2025-12-20 14:37:01 +0530

    Changed map storage to vector storage

--- Order_Book/include/order_book.h
+++ Order_Book/include/order_book.h
@@ -17,17 +17,27 @@ private:
     size_t trades;
     Order_Pool<Order> order_pool;
     std::vector<TradeRecord> trade_log;
-    std::map<double, std::list<Order*>, std::greater<double>> bids;   // Take the highest Bid First
-    std::map<double, std::list<Order*>, std::less<double>> asks;      // Take the lowest ask first
+
+    // std::map<double, std::list<Order*>, std::greater<double>> bids;   // Take the highest Bid First
+    // std::map<double, std::list<Order*>, std::less<double>> asks;      // Take the lowest ask first
+
+    std::vector<std::list<Order*>> bids;
+    std::vector<std::list<Order*>> asks;
+
     std::unordered_map<int, Order*> lookup_table;
 
 public:
+    int max_bid_tick = 0;
+    int min_ask_tick = 1e6-1;
+
     Order_Book() 
     : orders(1e5) 
     , trades(1e6)
     , order_pool(1e5)
     {
         trade_log.reserve(trades);
+        bids.resize(1e6);
+        asks.resize(1e6);
     }
 
     void addOrder(const int& id, const int& quantity, const double& price, const Side& side);
