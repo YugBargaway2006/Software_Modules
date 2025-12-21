f6ae610387aca8d35c14a5f29badd744d67d7b86
Yug Bargaway <yugbargawayskills@gmail.com>
2025-12-22 02:17:17 +0530

    Added receiver in entrypoint and updated cmakefile

--- Order_Book/main.cpp
+++ Order_Book/main.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-#include "../include/order_book.h"
-#include "../include/order.h"
+#include "include/order_book.h"
+#include "include/order.h"
+#include "include/market_data_receiver.h"
 
 int main() {
     Order_Book book;
 
-    std::cout << "--- 1. ADD BUY 100 @ 100 (ID 1) ---\n";
-    book.addOrder(1, 100, 100.0, Side::bid);
-    book.printOrderBook();
+    MarketDataReceiver receiver("239.0.0.1", 8080);
+    receiver.start(book);
 
-    std::cout << "--- 2: ADD BUY 50 @ 101 (ID 2) ---\n";
-    book.addOrder(2, 50, 101.0, Side::bid);
-    book.printOrderBook();
 
-    std::cout << "--- 3: ADD SELL 100 @ 102 (ID 3) ---\n";
-    book.addOrder(3, 100, 102.0, Side::ask);
-    book.printOrderBook();
+    // std::cout << "--- 1. ADD BUY 100 @ 100 (ID 1) ---\n";
+    // book.addOrder(1, 100, 100.0, Side::bid);
+    // book.printOrderBook();
 
-    std::cout << "--- 4: ADD SELL 100 @ 100 (ID 4) ---\n";
-    book.addOrder(4, 100, 100.0, Side::ask);
+    // std::cout << "--- 2: ADD BUY 50 @ 101 (ID 2) ---\n";
+    // book.addOrder(2, 50, 101.0, Side::bid);
+    // book.printOrderBook();
+
+    // std::cout << "--- 3: ADD SELL 100 @ 102 (ID 3) ---\n";
+    // book.addOrder(3, 100, 102.0, Side::ask);
+    // book.printOrderBook();
+
+    // std::cout << "--- 4: ADD SELL 100 @ 100 (ID 4) ---\n";
+    // book.addOrder(4, 100, 100.0, Side::ask);
     
-    std::cout << "\n--- FINAL BOOK STATE ---\n";
-    book.printOrderBook();
+    // std::cout << "\n--- FINAL BOOK STATE ---\n";
+    // book.printOrderBook();
 
     return 0;
 }
