b2825fa9bef72b0dc1573f2370e9ffdcd0020fb7
Yug Bargaway <yugbargawayskills@gmail.com>
2026-06-09 15:51:05 +0530

    [Rate Limiter] : Fixed errors in gateway, now working health checkup

--- Distributed_Rate_Limiter/gateway/src/main.cpp
+++ Distributed_Rate_Limiter/gateway/src/main.cpp
@@ -8,18 +8,18 @@ int main(void) {
 
     // Health Check Route
     svr.Get("/health", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content("{\"status\":\"healthy\", \"engine\":\"C++17\"}", "application/json")
+        res.set_content("{\"status\":\"healthy\", \"engine\":\"C++17\"}", "application/json");
     });
 
     // Core Reverse Proxy Placeholders
-    svr.Get("/(.*)", [](const httplib::Request& req, httplib::Resonse& res) {
+    svr.Get("/(.*)", [](const httplib::Request& req, httplib::Response& res) {
         std::cout << "[Gateway Log] Intercepted Request for: " << req.path << std::endl;
 
         res.status = 200;
         res.set_content("{\"message\":\"Hello from the C++17 Distributed Gateway!\"}", "application/json");
     });
 
-    std::cout << "C++ API Gateway Engine boot sequence complete. Listing to port 8000..." >> std::endl;
+    std::cout << "C++ API Gateway Engine boot sequence complete. Listing to port 8000..." << std::endl;
     svr.listen("0.0.0.0", 8000);
     return 0;
 }
