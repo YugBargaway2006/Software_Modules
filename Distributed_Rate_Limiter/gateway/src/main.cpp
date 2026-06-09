7939061ad3b552ba9de6fdecf0b56f05d04e5cc5
Yug Bargaway <yugbargawayskills@gmail.com>
2026-06-09 15:36:20 +0530

    [Rate Limiter] : Added custom c++ gateway with CMakeList and Dockerfile for compose and running

---
+++ Distributed_Rate_Limiter/gateway/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
+
+int main(void) {
+    httplib::Server svr;
+
+    // Health Check Route
+    svr.Get("/health", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("{\"status\":\"healthy\", \"engine\":\"C++17\"}", "application/json")
+    });
+
+    // Core Reverse Proxy Placeholders
+    svr.Get("/(.*)", [](const httplib::Request& req, httplib::Resonse& res) {
+        std::cout << "[Gateway Log] Intercepted Request for: " << req.path << std::endl;
+
+        res.status = 200;
+        res.set_content("{\"message\":\"Hello from the C++17 Distributed Gateway!\"}", "application/json");
+    });
+
+    std::cout << "C++ API Gateway Engine boot sequence complete. Listing to port 8000..." >> std::endl;
+    svr.listen("0.0.0.0", 8000);
+    return 0;
+}
