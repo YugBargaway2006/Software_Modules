d1c49218a843a71b2a5e4b619c6561921fc2f04f
Yug Bargaway <yugbargawayskills@gmail.com>
2026-06-10 12:12:43 +0530

    [Rate Limiter] : Lua Scripting and Token Bucket logic implemented

--- Distributed_Rate_Limiter/gateway/src/main.cpp
+++ Distributed_Rate_Limiter/gateway/src/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <chrono>
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
-#include <memory>
 #include <sw/redis++/redis++.h>
+
+
+// Atomic Token Bucket Script in Lua
+const std::string LUA_TOKEN_BUCKET = R"(
+    local key = KEYS[1]
+    local capacity = tonumber(ARGV[1])
+    local refill_rate = tonumber(ARGV[2])
+    local now = tonumber(ARGV[3])
+    local requested = tonumber(ARGV[4])
+
+    -- Fetch current bucket state
+    local data = redis.call('HMGET', key, 'tokens', 'last_updated')
+    local tokens = tonumber(data[1])
+    local last_updated = tonumber(data[2])
+
+    -- Initialize bucket if user dne
+    if not tokens then
+        tokens = capacity
+        last_updated = now
+    else 
+        local elapsed = now - last_updated
+        if elapsed > 0 then 
+            tokens = math.min(capacity, tokens + (elapsed * refill_rate))
+            last_updated = now
+        end 
+    end 
+
+    -- Rate Limiter Logic
+    if tokens >= requested then
+        tokens = tokens - requested 
+        redis.call('HMSET', key, 'tokens', tokens, 'last_updated', last_updated)
+        return 1    -- Allowed
+    else 
+        redis.call('HMSET', key, 'tokens', tokens, 'last_updated', last_updated)
+        return 0    -- Throtted (HTTP 429)
+    end
+)";
 
 
 int main(void) {
@@ -45,11 +84,46 @@ int main(void) {
         }
     });
 
-    // Generic Route
-    svr.Get("/(.*)", [](const httplib::Request& req, httplib::Response& res) {
-        std::cout << "[Gateway Log] Intercepted Request for: " << req.path << std::endl;
-        res.status = 200;
-        res.set_content("{\"message\":\"Hello from the C++17 Distributed Gateway!\"}", "application/json");
+    // Core Gateway Route
+    svr.Get("/(.*)", [redis_client](const httplib::Request& req, httplib::Response& res) {
+        std::string client_ip = req.remote_addr;
+        std::string redis_key = "rate_limit: " + client_ip;
+
+        // Rate Limiting Rules
+        std::string capacity = "5";
+        std::string refill_rate = "1";
+
+        auto now_seconds = std::to_string(std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count());
+            
+        std::string requested = "1";
+
+        try {
+            // Run lua script on redis server
+            // KEYS[1] = redis_key
+            // ARGV = {capacity, refill_rate, now_seconds, requested}
+            auto result = redis_client->eval<long long>(
+                LUA_TOKEN_BUCKET,
+                {redis_key},
+                {capacity, refill_rate, now_seconds, requested}
+            );
+
+            if (result == 1) {
+                res.status = 200;      // OK
+                res.set_content("{\"status\":\"Allowed\",\"client_ip\":\"" + client_ip + "\"}", "application/json");
+            } else {
+                res.status = 429;      // Too Many Requests
+                res.set_content("{\"error\":\"Too Many Requests\",\"message\":\"Rate limit exceeded. Bucket empty.\"}", "application/json");
+            }
+        }
+        catch (const std::exception& e) {
+            res.status = 500;
+            res.set_content("{\"error\":\"Internal Server Error\",\"details\":\"" + std::string(e.what()) + "\"}", "application/json");
+        }
+
+        // std::cout << "[Gateway Log] Intercepted Request for: " << req.path << std::endl;
+        // res.status = 200;
+        // res.set_content("{\"message\":\"Hello from the C++17 Distributed Gateway!\"}", "application/json");
     });
 
     std::cout << "C++ API Gateway Engine boot sequence complete. Listing to port 8000..." << std::endl;
