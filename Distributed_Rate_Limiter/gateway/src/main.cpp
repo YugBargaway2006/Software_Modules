4d4f4f9131b97ae6f99a879a875092d3ee5957df
Yug Bargaway <yugbargawayskills@gmail.com>
2026-06-10 11:37:24 +0530

    [Rate Limiter] : Added Redis Cache layer and modified dockerfile, compose and main cpp file to encorporate the changes

--- Distributed_Rate_Limiter/gateway/src/main.cpp
+++ Distributed_Rate_Limiter/gateway/src/main.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
+#include <memory>
+#include <sw/redis++/redis++.h>
 
 
 int main(void) {
     httplib::Server svr;
+
+    // Initialize Redis Client Connection
+    std::shared_ptr<sw::redis::Redis> redis_client;
+    try {
+        sw::redis::ConnectionOptions opts;
+        opts.host = "redis_state";    // Map directly to redis container
+        opts.port = 6379
+
+        sw::redis::ConnectionPoolOptions pool_opts;
+        pool_opts.size = 10;    // Max active connections in gateway pool
+
+        redis_client = std::make_shared<sw::redis::Redis>(opts, pool_opts);
+        std::cout << "Redis Pool initialized successfully" << std::endl;
+    } 
+    catch(const std::exception& e) {
+        std::cerr << "Error initializing Redis connection: " << e.what() << std::endl;
+        return 1;
+    }
 
     // Health Check Route
     svr.Get("/health", [](const httplib::Request&, httplib::Response& res) {
         res.set_content("{\"status\":\"healthy\", \"engine\":\"C++17\"}", "application/json");
     });
 
-    // Core Reverse Proxy Placeholders
+    // Verification Route to test Redis Cache
+    svr.Get("/redis-ping", [redis_client](const httplib::Request&, httplib::Response& res) {
+        try {
+            // Send a ping command
+            std::string pong = redis_client->ping();
+            res.status = 200;
+            res.set_content("{\"redis_status\":\"connected\",\"response\":\"" + pong + "\"}", "application/json");
+        }
+        catch (const sw::redis::Error& e) {
+            res.status = 500;
+            res.set_content("{\"redis_status\":\"disconnected\",\"error\":\"" + std::string(e.what()) + "\"}", "application/json");
+        }
+    });
+
+    // Generic Route
     svr.Get("/(.*)", [](const httplib::Request& req, httplib::Response& res) {
         std::cout << "[Gateway Log] Intercepted Request for: " << req.path << std::endl;
-
         res.status = 200;
         res.set_content("{\"message\":\"Hello from the C++17 Distributed Gateway!\"}", "application/json");
     });
